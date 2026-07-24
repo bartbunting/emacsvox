93958a05e2c063a9a89981c98febce476c95f5b3
Bart Bunting <bart@ursys.com.au>
2026-07-24 17:49:02 +1000

    Migrate Elisp Refs advice

--- test/run-tests.el
+++ test/run-tests.el
@@ -191,6 +191,7 @@
   (require 'emacsvox-ebuku-tests)
   (require 'emacsvox-ein-tests)
   (require 'emacsvox-elfeed-tests)
+  (require 'emacsvox-elisp-refs-tests)
   (require 'emacsvox-trace-tests))
 
 (ert-run-tests-batch-and-exit)
