22c037853d7206f7187915e10f9a9c2188a25847
Bart Bunting <bart@ursys.com.au>
2026-07-24 17:41:27 +1000

    Migrate Dumb Jump advice

--- test/run-tests.el
+++ test/run-tests.el
@@ -186,6 +186,7 @@
   (require 'emacsvox-corfu-tests)
   (require 'emacsvox-deadgrep-tests)
   (require 'emacsvox-devdocs-tests)
+  (require 'emacsvox-dumb-jump-tests)
   (require 'emacsvox-trace-tests))
 
 (ert-run-tests-batch-and-exit)
