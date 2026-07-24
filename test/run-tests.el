e8377327301b6c704a69396e7ada482ce247eb93
Bart Bunting <bart@ursys.com.au>
2026-07-24 18:17:08 +1000

    Migrate Helpful advice to native API

--- test/run-tests.el
+++ test/run-tests.el
@@ -207,6 +207,7 @@
   (require 'emacsvox-go-mode-tests)
   (require 'emacsvox-google-tests)
   (require 'emacsvox-haskell-tests)
+  (require 'emacsvox-helpful-tests)
   (require 'emacsvox-trace-tests))
 
 (ert-run-tests-batch-and-exit)
