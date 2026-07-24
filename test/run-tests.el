42da853c3be585a53b4aafd00b3275207b62823b
Bart Bunting <bart@ursys.com.au>
2026-07-24 18:28:38 +1000

    Migrate Muse advice to native API

--- test/run-tests.el
+++ test/run-tests.el
@@ -216,6 +216,7 @@
   (require 'emacsvox-mines-tests)
   (require 'emacsvox-mu4e-tests)
   (require 'emacsvox-multiple-cursors-tests)
+  (require 'emacsvox-muse-tests)
   (require 'emacsvox-trace-tests))
 
 (ert-run-tests-batch-and-exit)
