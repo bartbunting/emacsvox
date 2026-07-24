e01d59d0235e6758d5cae9f89809ba2f1974f266
Bart Bunting <bart@ursys.com.au>
2026-07-24 18:44:18 +1000

    Migrate Sudoku advice to native API

--- test/run-tests.el
+++ test/run-tests.el
@@ -232,6 +232,7 @@
   (require 'emacsvox-sdcv-tests)
   (require 'emacsvox-slime-tests)
   (require 'emacsvox-smartparens-tests)
+  (require 'emacsvox-sudoku-tests)
   (require 'emacsvox-trace-tests))
 
 (ert-run-tests-batch-and-exit)
