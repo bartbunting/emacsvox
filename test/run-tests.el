270b12af37a302b9c288e3563f5ce016c4e1663e
Bart Bunting <bart@ursys.com.au>
2026-07-24 17:41:01 +1000

    Migrate DevDocs advice

--- test/run-tests.el
+++ test/run-tests.el
@@ -185,6 +185,7 @@
   (require 'emacsvox-consult-tests)
   (require 'emacsvox-corfu-tests)
   (require 'emacsvox-deadgrep-tests)
+  (require 'emacsvox-devdocs-tests)
   (require 'emacsvox-trace-tests))
 
 (ert-run-tests-batch-and-exit)
